2d7db13894e3439deae9cca6d51a98bc9769ff20
Yuewen Wang <119701257+Xueling22@users.noreply.github.com>
2025-08-02 02:36:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-import pandas
-
+import pandas as pd
+ 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
